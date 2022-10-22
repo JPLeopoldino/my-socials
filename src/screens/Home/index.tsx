f6a3f698ab376223a8c3b106924e9895c4c54188
João Pedro <leopoldinodev@gmail.com>
2022-10-21 23:45:13 -0300

    feat: ensure initial Home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as SC from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <SC.Container>
+      <SC.Title>Hello World.</SC.Title>
+    </SC.Container>
+  );
+}
+
+export default Home;
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  flex: 1;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #FAFAFA;
+`
+
+export const Title = styled.h1`
+  font-size: 4rem;
+  color: #0F0F0F;
+  font-weight: bold;
+  text-align: center;
+`
