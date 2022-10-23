625b60c3d22eaeb0be3f63b99fb24e0155a5a52b
João Pedro <leopoldinodev@gmail.com>
2022-10-22 23:21:21 -0300

    feat: ensure cards and text components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import * as SC from './styles';
+import React from "react";
+import * as SC from "./styles";
 
 const Home: React.FC = () => {
   return (
     <SC.Container>
       <SC.Title>Hello World.</SC.Title>
+      <SC.Box>
+        <SC.Circle />
+        <SC.Heading>Lorem ipsum</SC.Heading>
+        <SC.Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </SC.Text>
+      </SC.Box>
     </SC.Container>
   );
-}
+};
 
 export default Home;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,3 +16,49 @@ export const Title = styled.h1`
   font-weight: bold;
   text-align: center;
 `
+
+export const Heading = styled.h3`
+  font-size: 1.5rem;
+  color: #0F0F0F;
+  font-weight: bold;
+  text-align: center;
+`
+
+export const Text = styled.p`
+  font-size: 1rem;
+  color: #0F0F0F;
+  text-align: center;
+`
+
+export const Circle = styled.div`
+  position: absolute;
+  top: -4rem;
+  width: 7rem;
+  height: 7rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #CACACA;
+  border: 0.4rem solid #FAFAFA;
+  border-radius: 100%;
+  z-index: 2;
+  cursor: pointer;
+`;
+
+export const Box = styled.div`
+  position: relative;
+  margin-top: 1rem;
+  padding: 2rem 1.5rem 1rem 1.5rem;
+  width: 16rem;
+  height: 20rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #CACACA;
+  border: 1px solid #AAAAAA;
+  border-radius: 20px;
+  box-shadow: -2px 5px 10px 0 rgba(0, 0, 0, 0.3);
+  z-index: 1;
+`
